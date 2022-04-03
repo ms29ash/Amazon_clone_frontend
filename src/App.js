ca9ec659bbfbe758b3dc9c16e7d6f33ac35dac31
ms29ash <ms29ash@gmail.com>
2022-04-04 00:17:57 +0530

    header created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './Header';
+
 
 function App() {
   return (
 
     <>
 
+      <Header />
     </>
 
   );
---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FiShoppingCart } from "react-icons/fi";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="https://pngimg.com/uploads/amazon/amazon_PNG25.png"
+        alt=""
+      />
+      <div className="header__search">
+        <input type="text" />
+        <AiOutlineSearch className="search__icon" />
+      </div>
+      <div className="nav__options">
+        <small>Hello , sign in</small>
+        <span>Accounts & Lists</span>
+      </div>
+      <div className="nav__options">
+        <small>Returns</small>
+        <span>& Orders</span>
+      </div>
+      <div className="nav__options">
+        <small>
+          <FiShoppingCart /> 0
+        </small>
+        <span>Cart</span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
